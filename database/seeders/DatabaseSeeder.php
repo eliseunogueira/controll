9390218f196d78e507cba8ec60dc0f01f92da0cb
root <root@controll.eliseunogueira.com.br>
2023-04-19 03:09:11 -0300

    chmod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Eliseu de Sousa Nogueira',
+	     'email' => 'eliseu.nogueira@outlook.com',
+	     'password' => bcrypt('pingwing')
+         ]);
     }
 }
